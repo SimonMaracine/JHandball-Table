1e386e4bfeea47b507ade40a34d1f6f5d972665f
SimonMaracine <simonmaracine@gmail.com>
2022-10-29 14:44:02 +0300

    Begin working on the main application's layout

---
+++ src/Application.java
@@ -0,0 +1,324 @@
+import javax.swing.*;
+import java.awt.*;
+
+import static java.awt.GridBagConstraints.BOTH;
+import static java.awt.GridBagConstraints.CENTER;
+import static javax.swing.ListSelectionModel.SINGLE_SELECTION;
+
+class Application extends JFrame {
+    private static final int MIN_WIDTH = 1024;
+    private static final int MIN_HEIGHT = 576;
+
+    private static final Font TEAM_NAME_FONT = new Font("Monospaced", Font.PLAIN, 30);
+    private static final Font TEAM_SCORE_FONT = new Font("Monospaced", Font.PLAIN, 55);
+    private static final Font TIMER_FONT = new Font("Monospaced", Font.PLAIN, 75);
+
+    private final JPanel pnlMain = new JPanel(new GridBagLayout());
+
+    private final JLabel lblLeftTeamName = new JLabel("Team 1");
+    private final JLabel lblRightTeamName = new JLabel("Team 2");
+    private final JLabel lblLeftTeamScore = new JLabel("0");
+    private final JLabel lblRightTeamScore = new JLabel("0");
+
+    private final DefaultListModel<String> leftTeamPlayers = new DefaultListModel<>();
+    private final DefaultListModel<String> rightTeamPlayers = new DefaultListModel<>();
+
+    private final JList<String> lstLeftTeamPlayers = new JList<>(leftTeamPlayers);
+    private final JList<String> lstRightTeamPlayers = new JList<>(rightTeamPlayers);
+
+    private final JPanel pnlSuspendedPlayers = new JPanel();
+    private final JLabel lblTimer = new JLabel("00:00");
+    private final JLabel lblSelectedPlayer = new JLabel("Selected player: None");
+
+//    private Player selectedPlayer = null;  // TODO create Player class
+
+    Application() {
+        super("JHandball Table");
+
+        setupLayout();
+
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setMinimumSize(new Dimension(MIN_WIDTH, MIN_HEIGHT));
+        setVisible(true);
+    }
+
+    private void setupLayout() {
+        setupMenuBar();
+        setupTeams();
+        setupTimer();
+        setupPlayerOptions();
+
+        add(pnlMain);
+        pack();
+    }
+
+    private void setupMenuBar() {
+        var menMenuBar = new JMenuBar();
+
+        var menMatch = new JMenu("Match");
+        var menPlayer = new JMenu("Player");
+        var menHelp = new JMenu("Help");
+
+        JMenuItem menItem;
+
+        menItem = new JMenuItem("Initialize");
+        menItem.addActionListener(actionEvent -> initializeMatch());
+        menMatch.add(menItem);
+
+        menItem = new JMenuItem("Begin");
+        menItem.addActionListener(actionEvent -> beginMatch());
+        menMatch.add(menItem);
+
+        menItem = new JMenuItem("End");
+        menItem.addActionListener(actionEvent -> endMatch());
+        menMatch.add(menItem);
+
+        menItem = new JMenuItem("Reset");
+        menItem.addActionListener(actionEvent -> resetMatch());
+        menMatch.add(menItem);
+
+        menItem = new JMenuItem("Score Down");
+        menItem.addActionListener(actionEvent -> scoreDownPlayer());
+        menPlayer.add(menItem);
+
+        menItem = new JMenuItem("Release");
+        menItem.addActionListener(actionEvent -> releasePlayer());
+        menPlayer.add(menItem);
+
+        menItem = new JMenuItem("Take Yellow Card");
+        menItem.addActionListener(actionEvent -> takeYellowCardPlayer());
+        menPlayer.add(menItem);
+
+        menItem = new JMenuItem("Take Red Card");
+        menItem.addActionListener(actionEvent -> takeRedCardPlayer());
+        menPlayer.add(menItem);
+
+        menHelp.add(new JMenuItem("About"));
+
+        menMenuBar.add(menMatch);
+        menMenuBar.add(menPlayer);
+        menMenuBar.add(menHelp);
+
+        setJMenuBar(menMenuBar);
+    }
+
+    private void setupTeams() {
+        var pnlTeams = new JPanel(new GridBagLayout());
+        var pnlLeftTeam = new JPanel(new GridBagLayout());
+        var pnlRightTeam = new JPanel(new GridBagLayout());
+        var pnlMiddle = new JPanel(new GridBagLayout());
+
+        var constraints = new GridBagConstraints(0, 0, 1, 1, 1.0, 1.0, CENTER, BOTH, new Insets(0, 0, 0, 0), 0, 0);
+
+        // Left team
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        pnlLeftTeam.add(lblLeftTeamName, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        pnlLeftTeam.add(lstLeftTeamPlayers, constraints);
+
+        // Right team
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        pnlRightTeam.add(lblRightTeamName, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        pnlRightTeam.add(lstRightTeamPlayers, constraints);
+
+        // Middle
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        pnlMiddle.add(lblLeftTeamScore, constraints);
+
+        constraints.gridx = 1;
+        constraints.gridy = 0;
+        pnlMiddle.add(lblRightTeamScore, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        constraints.gridwidth = 2;
+        pnlMiddle.add(pnlSuspendedPlayers, constraints);
+
+        constraints.gridwidth = 1;
+
+        // Add intermediate panels
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        pnlTeams.add(pnlLeftTeam, constraints);
+
+        constraints.gridx = 1;
+        constraints.gridy = 0;
+        pnlTeams.add(pnlMiddle, constraints);
+
+        constraints.gridx = 2;
+        constraints.gridy = 0;
+        pnlTeams.add(pnlRightTeam, constraints);
+
+        // Add teams panel
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.gridheight = 2;
+        constraints.weightx = 0.9;
+        constraints.weighty = 1.0;
+        pnlMain.add(pnlTeams, constraints);
+
+        lblLeftTeamName.setFont(TEAM_NAME_FONT);
+        lblRightTeamName.setFont(TEAM_NAME_FONT);
+        lblLeftTeamScore.setFont(TEAM_SCORE_FONT);
+        lblRightTeamScore.setFont(TEAM_SCORE_FONT);
+
+        lstLeftTeamPlayers.setSelectionMode(SINGLE_SELECTION);
+        lstRightTeamPlayers.setSelectionMode(SINGLE_SELECTION);
+    }
+
+    private void setupTimer() {
+        var pnlTimer = new JPanel(new GridBagLayout());
+
+        var btnLeftTeamTimeout = new JButton("Left Team Timeout");
+        var btnRightTeamTimeout = new JButton("Right Team Timeout");
+        var btnRefereeTimeout = new JButton("Referee Timeout");
+
+        var constraints = new GridBagConstraints(0, 0, 1, 1, 1.0, 1.0, CENTER, BOTH, new Insets(0, 0, 0, 0), 0, 0);
+
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.gridwidth = 3;
+        pnlTimer.add(lblTimer, constraints);
+
+        constraints.gridwidth = 1;
+
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        pnlTimer.add(btnLeftTeamTimeout, constraints);
+
+        constraints.gridx = 1;
+        constraints.gridy = 1;
+        pnlTimer.add(btnRightTeamTimeout, constraints);
+
+        constraints.gridx = 2;
+        constraints.gridy = 1;
+        pnlTimer.add(btnRefereeTimeout, constraints);
+
+        // Add timer panel
+        constraints.gridx = 1;
+        constraints.gridy = 0;
+        constraints.weightx = 0.2;
+        constraints.weighty = 0.2;
+        pnlMain.add(pnlTimer, constraints);
+
+        btnLeftTeamTimeout.addActionListener(actionEvent -> leftTeamTimeout());
+        btnRightTeamTimeout.addActionListener(actionEvent -> rightTeamTimeout());
+        btnRefereeTimeout.addActionListener(actionEvent -> refereeTimeout());
+
+        lblTimer.setFont(TIMER_FONT);
+    }
+
+    private void setupPlayerOptions() {
+        var pnlPlayerOptions = new JPanel(new GridBagLayout());
+
+        var btnScore = new JButton("Score");
+        var btnSuspend = new JButton("Suspend");
+        var btnYellowCard = new JButton("Yellow Card");
+        var btnRedCard = new JButton("Red Card");
+
+        var constraints = new GridBagConstraints(0, 0, 1, 1, 1.0, 1.0, CENTER, BOTH, new Insets(0, 0, 0, 0), 0, 0);
+
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.gridwidth = 2;
+        pnlPlayerOptions.add(lblSelectedPlayer, constraints);
+
+        constraints.gridwidth = 1;
+
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        pnlPlayerOptions.add(btnScore, constraints);
+
+        constraints.gridx = 1;
+        constraints.gridy = 1;
+        pnlPlayerOptions.add(btnSuspend, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 2;
+        pnlPlayerOptions.add(btnYellowCard, constraints);
+
+        constraints.gridx = 1;
+        constraints.gridy = 2;
+        pnlPlayerOptions.add(btnRedCard, constraints);
+
+        // Add player options panel
+        constraints.gridx = 1;
+        constraints.gridy = 1;
+        constraints.weightx = 0.1;
+        constraints.weighty = 1.0;
+        pnlMain.add(pnlPlayerOptions, constraints);
+
+        btnScore.addActionListener(actionEvent -> scoreUpPlayer());
+        btnSuspend.addActionListener(actionEvent -> suspendPlayer());
+        btnYellowCard.addActionListener(actionEvent -> giveYellowCardPlayer());
+        btnRedCard.addActionListener(actionEvent -> giveRedCardPlayer());
+    }
+
+    private void initializeMatch() {
+
+    }
+
+    private void beginMatch() {
+
+    }
+
+    private void endMatch() {
+
+    }
+
+    private void resetMatch() {
+
+    }
+
+    private void scoreDownPlayer() {
+
+    }
+
+    private void releasePlayer() {
+
+    }
+
+    private void takeYellowCardPlayer() {
+
+    }
+
+    private void takeRedCardPlayer() {
+
+    }
+
+    private void leftTeamTimeout() {
+
+    }
+
+    private void rightTeamTimeout() {
+
+    }
+
+    private void refereeTimeout() {
+
+    }
+
+    private void scoreUpPlayer() {
+
+    }
+
+    private void suspendPlayer() {
+
+    }
+
+    private void giveYellowCardPlayer() {
+
+    }
+
+    private void giveRedCardPlayer() {
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        String[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
+        System.out.println(Arrays.toString(fonts));
+
+        Font defaultFont = new Font("Monospaced", Font.PLAIN, 14);
+
+        UIManager.put("Button.font", defaultFont);
+        UIManager.put("Label.font", defaultFont);
+        UIManager.put("List.font", defaultFont);
+        UIManager.put("TextField.font", defaultFont);
+        UIManager.put("MenuBar.font", defaultFont);
+        UIManager.put("Menu.font", defaultFont);
+        UIManager.put("MenuItem.font", defaultFont);
+
+        SwingUtilities.invokeLater(Application::new);
     }
 }
